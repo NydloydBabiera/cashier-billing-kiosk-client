64964c39fac197c00c8c47f90f8f7dfd7ba635cf
NydloydBabiera <nydloyd.babiera2998@gmail.com>
2024-11-17 07:27:35 +0800

    remove border in dashboard

--- src/pages/BillingDashboard.jsx
+++ src/pages/BillingDashboard.jsx
@@ -4,7 +4,7 @@ import RFIDReader from "../components/RFIDReader";
 import icon from "../assets/coin-bill-acceptor logo.png";
 const BillingDashboard = () => {
   return (
-    <div className="flex mb-4 border border-black h-screen">
+    <div className="flex mb-4 h-screen">
       <div className="w-1/2 bg-blue-500 content-center place-items-center">
         <div className="place-items-center bg-blue-500 rounded-md w-80 ">
           <h1 className="text-white font-handwriting text-5xl mx-5 place-items-center">
